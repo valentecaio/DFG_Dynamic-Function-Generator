2402e53031d4b3616a7ecce0877b496146f8e876
Caio Valente <caio.valente@mindsatwork.com.br>
2015-11-11 17:41:33 -0200

    teste de assembly de push de inteiros

--- funcao-desejada.s
+++ funcao-desejada.s
@@ -1,6 +1,10 @@
 /*
 	Esse arquivo é onde a gente vai criar funções em assembly para fazer objdmp e descobrir os codigos
 	de maquina de cada comando.
+	
+	Para fazer objdump:
+	gcc -m32 -c funcao-desejada.s
+	objdump -d funcao-desejada.o	
 */
 
 .text
@@ -10,9 +14,9 @@ assemblyFoo:
 	push	%ebp
 	movl	%esp, %ebp
 	
-	pushl	12(%ebp)	# empurra o parametro da direita (float ou inteiro, 32 bits)
-	pushl	8(%ebp)		# empurra o parametro da esquerda (float ou inteiro, 32 bits)
-	call	pow
+	pushl	12(%ebp)	# empurra o parametro da direita (inteiro, 32 bits)
+	pushl	8(%ebp)		# empurra o parametro da esquerda (inteiro, 32 bits)
+	call	soma
 	
 	movl	%ebp, %esp
 	popl	%ebp
--- teste-assembly.c
+++ teste-assembly.c
@@ -1,14 +1,27 @@
+/*
+ * arquivo para testar a funcao-desejada.s
+ * gcc -Wall -m32 -o funcao-desejada funcao-desejada.s teste-assembly.c -lm
+ * 
+ * */
+
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "cria_func.h"
 
 typedef int (*funcp) (int);
 typedef int (*funcp2) (funcp, int);
 
+int assemblyFoo (int a, int b);
+
+int soma (int a, int b) {
+	return a+b;
+}
+
 int main (void) {
-	float a;
-	printf("\n\nTestando assembly: ");
-	a = assemblyFoo(2.0,4.0);
-	printf ("%f", a);
+	int a, b=2, c=4;
+	printf("\n\nTestando assembly: \n\n");
+	a = assemblyFoo(b,c);
+	printf ("Desejado: %d, \tobtido: %d\n\n", soma(b,c), a);
 	return 0;
 }
