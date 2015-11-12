af85f6779280dd7998ae28263da6e8f5ceae445d
Caio Valente <caio.valente@mindsatwork.com.br>
2015-11-11 22:10:33 -0200

    altera valor das variaveis de teste

--- teste.c
+++ teste.c
@@ -12,17 +12,17 @@ int soma (int a, int b) {
 
 int main (void) {
 	DescParam params[2];
-	int b=2,c=4;
+	int p0=2000,p1=4;
 	funcp f_gerada = NULL;
 
 	params[0].tipo_val = INT_PAR;
 	params[0].orig_val = FIX_DIR;
-	params[0].valor.v_int = b;
+	params[0].valor.v_int = p0;
 	params[1].tipo_val = INT_PAR;
 	params[1].orig_val = FIX_DIR;
-	params[1].valor.v_int = c;
+	params[1].valor.v_int = p1;
 
 	f_gerada = (funcp) cria_func (soma, params, 2);
-	printf("\nTESTE EXECUTADO\n\nesperado: %d, obtido: %d\n\n", soma(b,c), f_gerada());
+	printf("\nTESTE EXECUTADO\n\nesperado: %d, obtido: %d\n\n", soma(p0,p1), f_gerada());
 	return 0;
 }
